076bd2fbdaa22beb74950b6099a3fe87008690f7
Mark Stickling <mkstix6@gmail.com>
2021-11-18 02:54:03 +0000

    Reorganise art code for easier SvelteKit portability

--- spiroflare/spiroflare.ts
+++ spiroflare/spiroflare.ts
@@ -1,214 +1,254 @@
 import { pseudoRandom } from "./utils";
 
-const c = <HTMLCanvasElement>document.getElementById("canvas");
-const ctx = <CanvasRenderingContext2D>c.getContext("2d");
-let frameNumber = 0;
+const canvasElement = <HTMLCanvasElement>document.getElementById("canvas");
+let ctx;
 
-const canvasSize = 2 ** 11;
-c.width = canvasSize;
-c.height = canvasSize;
-
-let hardTime = 0;
 let seed = Math.ceil(Math.random() * 100);
-document.getElementById("seed").innerHTML = `${seed}`;
+let preset: number = 3;
+
+const renderSize = 2 ** 11;
+let options;
+let hardTime = 0;
+let frameNumber = 0;
+let globalAlpha = 1;
+const colors: [number, string][] = [];
+const variances: number[] = [];
+let coverage: number;
+let radius: number;
+
+function startDrawing() {
+  canvasElement.width = renderSize;
+  canvasElement.height = renderSize;
+  ctx = <CanvasRenderingContext2D>canvasElement.getContext("2d");
+
+  // Prep a bunch of random numbers so they're stable as we add more presets
+  let optionsPRDs = [];
+  for (let i = 0; i < 100; i++) {
+    optionsPRDs.push(pseudoRandomDecimal());
+  }
+
+  const artStyles = [
+    {
+      name: "NeonFossils",
+      clearBetweenFrames: false,
+      fadeAlpha: false,
+      fadeAlphaRate: 0.001,
+      stopAtZeroWidth: true,
+      concentricLines: false,
+      coverageChange: 0.999995,
+      coverageStart: canvasElement.width * 0.2,
+      rotateMagnitude: 0.2,
+      actorCount: 10,
+      globalCompositeOperation: "source-over",
+      colorRange: 360 * optionsPRDs[0],
+      colorHueShift: 360 * optionsPRDs[1],
+      actorStepDistance: 10,
+      actorStepsPerFrame: 200,
+      lineRadiusStart: 4,
+      lineRadiusChangeRate: 0.99999,
+      arcPosition(time, coverage, variance): [number, number] {
+        return [
+          canvasElement.width / 2 +
+            Math.sin(time * 0.000153245 + variance * 2345645) *
+              coverage *
+              variance +
+            Math.sin(time * 0.00174565342 + variance * 3.73645) *
+              coverage *
+              variance,
+          canvasElement.height / 2 +
+            Math.cos(time * 0.000132546 + variance * 76456543) *
+              coverage *
+              variance +
+            Math.cos(time * 0.0012536345 + variance * 2.65484564) *
+              coverage *
+              variance,
+        ];
+      },
+    },
+    {
+      name: "StarBurst",
+      clearBetweenFrames: false,
+      fadeAlpha: false,
+      fadeAlphaRate: 0.001,
+      stopAtZeroWidth: true,
+      concentricLines: false,
+      coverageChange: 1,
+      coverageStart: canvasElement.width * 0.3,
+      rotateMagnitude: 0.2,
+      actorCount: 10,
+      globalCompositeOperation: "source-over",
+      colorRange: 360 * optionsPRDs[0],
+      colorHueShift: 360 * optionsPRDs[1],
+      actorStepDistance: 10,
+      actorStepsPerFrame: 200,
+      lineRadiusStart: 4,
+      lineRadiusChangeRate: 0.99999,
+      arcPosition(time, coverage, variance): [number, number] {
+        return [
+          canvasElement.width / 2 + Math.sin(time * 0.001) * coverage,
+          canvasElement.height / 2 + Math.sin(time * 0.001) * coverage,
+        ];
+      },
+    },
+    {
+      name: "TorusSpectre",
+      clearBetweenFrames: false,
+      maxFrames: 80,
+      fadeAlpha: false,
+      fadeAlphaRate: 0.001,
+      stopAtZeroWidth: true,
+      concentricLines: false,
+      coverageChange: 0.999999,
+      coverageStart: canvasElement.width * 0.3,
+      rotateMagnitude: 0.04,
+      actorCount: 3,
+      globalCompositeOperation: "source-over",
+      colorRange: 360 * optionsPRDs[0],
+      colorHueShift: 360 * optionsPRDs[1],
+      actorStepDistance: 15,
+      actorStepsPerFrame: 60,
+      lineRadiusStart: 4,
+      lineRadiusChangeRate: 1,
+      arcPosition(time, coverage, variance): [number, number] {
+        // prettier-ignore
+        return [
+        canvasElement.width / 2 + Math.sin((time * 0.001+variance*500) * Math.PI * 2 * (1 / 2) ) * coverage,
+        canvasElement.height / 2 + Math.cos((time * 0.001+variance*500) * Math.PI * 2 * (1 / 6)) * coverage,
+      ];
+      },
+    },
+    {
+      name: "PetalNebula",
+      clearBetweenFrames: false,
+      maxFrames: undefined,
+      fadeAlpha: false,
+      fadeAlphaRate: 0.001,
+      stopAtZeroWidth: true,
+      concentricLines: false,
+      coverageChange: 0.999997,
+      coverageStart: canvasElement.width * 0.73,
+      rotateMagnitude: 0.007,
+      actorCount: 7,
+      globalCompositeOperation: "source-over",
+      colorRange: 360 * optionsPRDs[0],
+      colorHueShift: 360 * optionsPRDs[1],
+      actorStepDistance: 10,
+      actorStepsPerFrame: 400,
+      lineRadiusStart: 4,
+      lineRadiusChangeRate: 1,
+      arcPosition(time, coverage, variance): [number, number] {
+        // prettier-ignore
+        return [
+        canvasElement.width / 2 + Math.sin(time*0.002 + variance * 6523) * coverage * variance*0.4
+        + Math.sin(time*0.001142) * coverage * 0.3,
+        canvasElement.height / 2 + Math.cos(time*0.002 + variance * 2543) * coverage * variance*0.4
+        + Math.cos(time*0.001142) * coverage * 0.3,
+      ];
+      },
+    },
+    {
+      name: "WarpFlower",
+      clearBetweenFrames: false,
+      maxFrames: 400,
+      fadeAlpha: true,
+      fadeAlphaRate: 0.015,
+      stopAtZeroWidth: true,
+      concentricLines: true,
+      globalVariablesAdjustPer: "frame",
+      coverageChange: 10,
+      coverageStart: canvasElement.width * 0.59,
+      rotateMagnitude: Math.PI / (1 + Math.ceil(optionsPRDs[2] * 5) * 0.5),
+      actorCount: 3,
+      globalCompositeOperation: "source-over",
+      colorRange: 250 * optionsPRDs[0],
+      colorHueShift: 360 * optionsPRDs[1],
+      actorStepDistance: 20,
+      actorStepsPerFrame: 150,
+      lineRadiusStart: 4,
+      lineRadiusChangeRate: 0.999,
+      arcPosition(time, coverage, variance): [number, number] {
+        // prettier-ignore
+        return [
+        canvasElement.width / 2 + Math.sin(time*0.002 + variance * 6523) * coverage * variance*0.4
+        + Math.sin(time*0.0017564) * coverage * 0.3,
+        canvasElement.height / 2 + Math.cos(time*0.002 + variance * 2543) * coverage * variance*0.4
+        + Math.cos(time*0.00016111) * coverage * 0.3,
+      ];
+      },
+    },
+    {
+      name: "SpiroCoil",
+      clearBetweenFrames: false,
+      maxFrames: undefined,
+      fadeAlpha: false,
+      fadeAlphaRate: 0.001,
+      stopAtZeroWidth: true,
+      concentricLines: false,
+      coverageChange: 0.999997,
+      coverageStart: canvasElement.width * 0.65,
+      rotateMagnitude: 0.1,
+      actorCount: 7,
+      globalCompositeOperation: "source-over",
+      colorRange: 360 * optionsPRDs[0],
+      colorHueShift: 360 * optionsPRDs[1],
+      actorStepDistance: 10,
+      actorStepsPerFrame: 400,
+      lineRadiusStart: 4,
+      lineRadiusChangeRate: 0.99995,
+      arcPosition(time, coverage, variance): [number, number] {
+        // prettier-ignore
+        return [
+        canvasElement.width / 2 + Math.sin(time*0.002 + variance * 6523) * coverage * variance*0.4
+        + Math.sin(time*0.001142) * coverage * 0.3,
+        canvasElement.height / 2 + Math.cos(time*0.002 + variance * 2543) * coverage * variance*0.4
+        + Math.cos(time*0.001142) * coverage * 0.3,
+      ];
+      },
+    },
+  ];
+
+  options = artStyles[preset];
+  coverage = options.coverageStart;
+  radius = options.lineRadiusStart;
+
+  // Populate art info into page
+  document.getElementById("seed").innerHTML = `${seed}`;
+  document.getElementById("artname").innerHTML = `${options.name}`;
+
+  // Create list of particle differences
+  for (let i = 0; i < options.actorCount; i++) {
+    variances.push(pseudoRandomDecimal());
+  }
+
+  // Create color list
+  for (
+    let i = -options.actorStepsPerFrame / 2;
+    i < options.actorStepsPerFrame / 2;
+    i++
+  ) {
+    let linearDecimalPeak =
+      1 - Math.abs(i) * (1 / (options.actorStepsPerFrame / 2));
+    let hue =
+      i * (options.colorRange / options.actorStepsPerFrame) +
+      options.colorHueShift;
+    let saturation = 100;
+    let lightness = linearDecimalPeak * 80 + 40;
+    let alpha = linearDecimalPeak;
+    colors.push([
+      i * options.actorStepDistance,
+      `hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`,
+    ]);
+  }
+
+  // Start
+  fillWithBlack();
+  draw();
+}
 
 let generator = pseudoRandom(seed);
-
 function pseudoRandomDecimal() {
   return parseFloat(`0.${generator.next().value.toString().slice(-5)}`);
 }
-// Prep a bunch of random numbers so they're stable as we add more presets
-let optionsPRDs = [];
-for (let i = 0; i < 100; i++) {
-  optionsPRDs.push(pseudoRandomDecimal());
-}
-
-const artStyles = [
-  {
-    name: "NeonFossils",
-    clearBetweenFrames: false,
-    fadeAlpha: false,
-    fadeAlphaRate: 0.001,
-    stopAtZeroWidth: true,
-    concentricLines: false,
-    coverageChange: 0.999995,
-    coverageStart: c.width * 0.2,
-    rotateMagnitude: 0.2,
-    actorCount: 10,
-    globalCompositeOperation: "source-over",
-    colorRange: 360 * optionsPRDs[0],
-    colorHueShift: 360 * optionsPRDs[1],
-    actorStepDistance: 10,
-    actorStepsPerFrame: 200,
-    lineRadiusStart: 4,
-    lineRadiusChangeRate: 0.99999,
-    arcPosition(time, coverage, variance): [number, number] {
-      return [
-        c.width / 2 +
-          Math.sin(time * 0.000153245 + variance * 2345645) *
-            coverage *
-            variance +
-          Math.sin(time * 0.00174565342 + variance * 3.73645) *
-            coverage *
-            variance,
-        c.height / 2 +
-          Math.cos(time * 0.000132546 + variance * 76456543) *
-            coverage *
-            variance +
-          Math.cos(time * 0.0012536345 + variance * 2.65484564) *
-            coverage *
-            variance,
-      ];
-    },
-  },
-  {
-    name: "StarBurst",
-    clearBetweenFrames: false,
-    fadeAlpha: false,
-    fadeAlphaRate: 0.001,
-    stopAtZeroWidth: true,
-    concentricLines: false,
-    coverageChange: 1,
-    coverageStart: c.width * 0.3,
-    rotateMagnitude: 0.2,
-    actorCount: 10,
-    globalCompositeOperation: "source-over",
-    colorRange: 360 * optionsPRDs[0],
-    colorHueShift: 360 * optionsPRDs[1],
-    actorStepDistance: 10,
-    actorStepsPerFrame: 200,
-    lineRadiusStart: 4,
-    lineRadiusChangeRate: 0.99999,
-    arcPosition(time, coverage, variance): [number, number] {
-      return [
-        c.width / 2 + Math.sin(time * 0.001) * coverage,
-        c.height / 2 + Math.sin(time * 0.001) * coverage,
-      ];
-    },
-  },
-  {
-    name: "TorusSpectre",
-    clearBetweenFrames: false,
-    maxFrames: 80,
-    fadeAlpha: false,
-    fadeAlphaRate: 0.001,
-    stopAtZeroWidth: true,
-    concentricLines: false,
-    coverageChange: 0.999999,
-    coverageStart: c.width * 0.3,
-    rotateMagnitude: 0.04,
-    actorCount: 3,
-    globalCompositeOperation: "source-over",
-    colorRange: 360 * optionsPRDs[0],
-    colorHueShift: 360 * optionsPRDs[1],
-    actorStepDistance: 15,
-    actorStepsPerFrame: 60,
-    lineRadiusStart: 4,
-    lineRadiusChangeRate: 1,
-    arcPosition(time, coverage, variance): [number, number] {
-      // prettier-ignore
-      return [
-        c.width / 2 + Math.sin((time * 0.001+variance*500) * Math.PI * 2 * (1 / 2) ) * coverage,
-        c.height / 2 + Math.cos((time * 0.001+variance*500) * Math.PI * 2 * (1 / 6)) * coverage,
-      ];
-    },
-  },
-  {
-    name: "PetalNebula",
-    clearBetweenFrames: false,
-    maxFrames: undefined,
-    fadeAlpha: false,
-    fadeAlphaRate: 0.001,
-    stopAtZeroWidth: true,
-    concentricLines: false,
-    coverageChange: 0.999997,
-    coverageStart: c.width * 0.73,
-    rotateMagnitude: 0.007,
-    actorCount: 7,
-    globalCompositeOperation: "source-over",
-    colorRange: 360 * optionsPRDs[0],
-    colorHueShift: 360 * optionsPRDs[1],
-    actorStepDistance: 10,
-    actorStepsPerFrame: 400,
-    lineRadiusStart: 4,
-    lineRadiusChangeRate: 1,
-    arcPosition(time, coverage, variance): [number, number] {
-      // prettier-ignore
-      return [
-        c.width / 2 + Math.sin(time*0.002 + variance * 6523) * coverage * variance*0.4
-        + Math.sin(time*0.001142) * coverage * 0.3,
-        c.height / 2 + Math.cos(time*0.002 + variance * 2543) * coverage * variance*0.4
-        + Math.cos(time*0.001142) * coverage * 0.3,
-      ];
-    },
-  },
-  {
-    name: "WarpFlower",
-    clearBetweenFrames: false,
-    maxFrames: 400,
-    fadeAlpha: true,
-    fadeAlphaRate: 0.015,
-    stopAtZeroWidth: true,
-    concentricLines: true,
-    globalVariablesAdjustPer: "frame",
-    coverageChange: 10,
-    coverageStart: c.width * 0.59,
-    rotateMagnitude: Math.PI / (1 + Math.ceil(optionsPRDs[2] * 5) * 0.5),
-    actorCount: 3,
-    globalCompositeOperation: "source-over",
-    colorRange: 250 * optionsPRDs[0],
-    colorHueShift: 360 * optionsPRDs[1],
-    actorStepDistance: 20,
-    actorStepsPerFrame: 150,
-    lineRadiusStart: 4,
-    lineRadiusChangeRate: 0.999,
-    arcPosition(time, coverage, variance): [number, number] {
-      // prettier-ignore
-      return [
-        c.width / 2 + Math.sin(time*0.002 + variance * 6523) * coverage * variance*0.4
-        + Math.sin(time*0.0017564) * coverage * 0.3,
-        c.height / 2 + Math.cos(time*0.002 + variance * 2543) * coverage * variance*0.4
-        + Math.cos(time*0.00016111) * coverage * 0.3,
-      ];
-    },
-  },
-  {
-    name: "SpiroCoil",
-    clearBetweenFrames: false,
-    maxFrames: undefined,
-    fadeAlpha: false,
-    fadeAlphaRate: 0.001,
-    stopAtZeroWidth: true,
-    concentricLines: false,
-    coverageChange: 0.999997,
-    coverageStart: c.width * 0.65,
-    rotateMagnitude: 0.1,
-    actorCount: 7,
-    globalCompositeOperation: "source-over",
-    colorRange: 360 * optionsPRDs[0],
-    colorHueShift: 360 * optionsPRDs[1],
-    actorStepDistance: 10,
-    actorStepsPerFrame: 400,
-    lineRadiusStart: 4,
-    lineRadiusChangeRate: 0.99995,
-    arcPosition(time, coverage, variance): [number, number] {
-      // prettier-ignore
-      return [
-        c.width / 2 + Math.sin(time*0.002 + variance * 6523) * coverage * variance*0.4
-        + Math.sin(time*0.001142) * coverage * 0.3,
-        c.height / 2 + Math.cos(time*0.002 + variance * 2543) * coverage * variance*0.4
-        + Math.cos(time*0.001142) * coverage * 0.3,
-      ];
-    },
-  },
-];
-
-const options = artStyles[2];
-document.getElementById("artname").innerHTML = `${options.name}`;
-
-let globalAlpha = 1;
-const colorRange = options.colorRange;
-const hueShift = options.colorHueShift;
 
 function draw(time: number = 0) {
   hardTime += 16;
@@ -243,14 +283,9 @@ function adjustGlobalVariables() {
 }
 
 function rotateCanvas() {
-  ctx.translate(c.width / 2, c.height / 2);
+  ctx.translate(canvasElement.width / 2, canvasElement.height / 2);
   ctx.rotate(options.rotateMagnitude);
-  ctx.translate(-c.width / 2, -c.height / 2);
-}
-
-const variances: number[] = [];
-for (let i = 0; i < options.actorCount; i++) {
-  variances.push(pseudoRandomDecimal());
+  ctx.translate(-canvasElement.width / 2, -canvasElement.height / 2);
 }
 
 function drawParticles(time: number) {
@@ -263,24 +298,6 @@ function drawParticles(time: number) {
   });
 }
 
-const colors: [number, string][] = [];
-for (
-  let i = -options.actorStepsPerFrame / 2;
-  i < options.actorStepsPerFrame / 2;
-  i++
-) {
-  let linearDecimalPeak =
-    1 - Math.abs(i) * (1 / (options.actorStepsPerFrame / 2));
-  let hue = i * (colorRange / options.actorStepsPerFrame) + hueShift;
-  let saturation = 100;
-  let lightness = linearDecimalPeak * 80 + 40;
-  let alpha = linearDecimalPeak;
-  colors.push([
-    i * options.actorStepDistance,
-    `hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`,
-  ]);
-}
-
 function drawParticle(
   time: number,
   variance: number = 2.6432557,
@@ -290,8 +307,6 @@ function drawParticle(
   drawCircle(time + tDiff, color, variance);
 }
 
-let coverage = options.coverageStart;
-let radius = options.lineRadiusStart;
 function drawCircle(time: number, color: string, variance: number) {
   ctx.beginPath();
   if (
@@ -310,8 +325,7 @@ function drawCircle(time: number, color: string, variance: number) {
 function fillWithBlack() {
   ctx.globalCompositeOperation = "source-over";
   ctx.fillStyle = "#000";
-  ctx.fillRect(0, 0, c.width, c.height);
+  ctx.fillRect(0, 0, canvasElement.width, canvasElement.height);
 }
 
-fillWithBlack();
-draw(0);
+startDrawing();
