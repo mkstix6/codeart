c69ea359dedaffa34b94ce915733ba2c9e466d37
Mark Stickling <mkstix6@gmail.com>
2022-02-07 22:12:58 +0000

    Refraction Field art code refactor

--- refractionfield/src/refractionfield.ts
+++ refractionfield/src/refractionfield.ts
@@ -4,110 +4,37 @@
 // - give each a reflective direction (think 'normal map' in 3D)
 // - change color depending on difference in angle from pointer to triangle's reflective direction.
 
+// Universal constants
 const TAU = Math.PI * 2;
 
-let mouseX = window.innerWidth * 0.5;
-let mouseY = window.innerHeight * 0.5;
+// Artwork variables
+const gridSize = 40;
+const hueRange = 160;
+const hueShift = 170;
 
+// Prepare canvas
 const canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 ctx.globalCompositeOperation = "source-over";
 
-const gridSize = 30;
-const hueRange = 160;
-const hueShift = 170;
+// Initialise pointer position to the center of the canvas
+let mouseX = window.innerWidth * 0.5;
+let mouseY = window.innerHeight * 0.5;
 
-let pointerLoopRAF;
-function pointerLoop(t) {
-  const radiusRatio = 0.35;
-  const radius = canvas.width * radiusRatio;
-  const rate = 0.001;
-  const x =
-    canvas.width * radiusRatio * Math.sin(t * rate) + canvas.width * 0.5;
-  const y =
-    canvas.height * radiusRatio * Math.cos(t * rate) + canvas.height * 0.5;
-  mouseX = x;
-  mouseY = y;
-  pointerLoopRAF = window.requestAnimationFrame(pointerLoop);
-}
-pointerLoop();
+const gridCoordinates = gridPoints(gridSize, "isometric");
 
-const blends = [
-  "source-over",
-  "source-atop",
-  // "source-in",
-  // "source-out",
-  // "destination-over",
-  // "destination-atop",
-  // "destination-in",
-  // "destination-out",
-  // "lighter",
-  // "copy",
-  "xor",
-  "multiply",
-  "darken",
-  // "lighten",
-  // "screen", // 🤷‍
-  // "color-dodge",
-  // "color-burn", // 🤷‍
-  // "hard-light",
-  // "soft-light", // 🤷‍
-  // "difference", // 🤮
-  // "exclusion", // 🤮
-  // "hue", // 🤷‍
-  // "saturation", // 🤷‍
-  // "color", // 🤷‍
-  // "luminosity", // 🤮
-  // "overlay", // 🤷‍
-];
-
-function gridPoints(type = "square") {
-  // type 'square', 'isometric'
-  const coordinates = [];
-
-  let dy = gridSize;
-  if (type === "isometric") {
-    dy = Math.sin((TAU * 60) / 360) * gridSize;
-  }
-
-  for (let i = 0; i - 1 < canvas.height / dy; i++) {
-    for (let j = 0; j - 1 < canvas.width / gridSize; j++) {
-      let x = j * gridSize;
-      if (type === "isometric") {
-        x = !!(i % 2) ? j * gridSize : j * gridSize + gridSize * 0.5;
-      }
-      let y = i * dy;
-      coordinates.push({ x, y, ix: j, iy: i });
-    }
-  }
-
-  return coordinates;
-}
-
-function drawGrid(points) {
-  points.forEach(({ x, y }) => {
-    ctx.beginPath();
-    ctx.arc(x, y, 2, 0, TAU);
-    ctx.strokeStyle = "black";
-    ctx.stroke();
-    ctx.closePath();
-  });
-}
-
-const gridCoordinates = gridPoints("isometric");
-
-// Fill canva
+// Fill canvas
 function clearCanvas() {
   ctx.globalCompositeOperation = "source-over";
   ctx.fillStyle = "#fff";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.globalCompositeOperation = blends[3];
+  ctx.globalCompositeOperation = "multiply";
 }
 
-// Draw triangle
-const triangles = gridCoordinates.map((position, index) => {
+// Prepare triangle data
+const trianglesData = gridCoordinates.map((position, index) => {
   const size = gridSize * 0.87;
   const triangle = {
     x: position.x,
@@ -125,71 +52,113 @@ const triangles = gridCoordinates.map((position, index) => {
   return triangle;
 });
 
-function draw(t) {
-  //   FIXME redundant variables to replace
-  let mx = mouseX;
-  let my = mouseY;
-
+// Draw triangle
+function drawArtwork(t = 0) {
   clearCanvas();
-
-  ctx.strokeStyle = "#fff8";
-  ctx.fillStyle = "#fff3";
-
-  triangles.forEach((triangle, tIndex) => {
-    // Compute cursor angle from normal
-    const { x, y, normal, size, spin, tHueShift } = triangle;
-    triangle.normal += spin;
-
-    const dx = mx - x;
-    const dy = my - y;
-    const dAngle = Math.atan2(dy, dx);
-
-    const angle = Math.abs(
-      Math.atan2(Math.sin(normal - dAngle), Math.cos(normal - dAngle))
-    );
-
-    const distance = Math.sqrt(dx ** 2 + dy ** 2);
-    const luminocity = canvas.width * 0.75;
-    const minDistanceFactor = 0.2;
-    let distanceFactor = (luminocity - distance) / luminocity;
-    distanceFactor =
-      distanceFactor < minDistanceFactor ? minDistanceFactor : distanceFactor;
-    const reflectiveAlignment = ((Math.PI - angle) / Math.PI) * distanceFactor;
-    //     Triangle
-    ctx.beginPath();
-    ctx.lineTo(x + size * Math.sin(normal), y + size * Math.cos(normal));
-    ctx.lineTo(
-      x + size * Math.sin(normal + TAU * 0.333),
-      y + size * Math.cos(normal + TAU * 0.333)
-    );
-    ctx.lineTo(
-      x + size * Math.sin(normal + TAU * 0.666),
-      y + size * Math.cos(normal + TAU * 0.666)
-    );
-    ctx.closePath();
-    // Fill
-    const hue = (angle / Math.PI) * hueRange + hueShift + tHueShift;
-    const sauration = 90;
-    const lightness = (reflectiveAlignment * 0.4 + 0.55) * 100;
-    const alpha = reflectiveAlignment;
-    ctx.fillStyle = `hsla(${hue},${sauration}%,${lightness}%,${alpha})`;
-    ctx.fill();
-  });
-
-  window.requestAnimationFrame(draw);
+  trianglesData.forEach(drawTriangle);
+  window.requestAnimationFrame(drawArtwork);
 }
 
+/// the main function
+drawArtwork();
+
+function drawTriangle(triangle, tIndex) {
+  const { x, y, normal, size, spin, tHueShift } = triangle;
+  // Increment triangle orientation
+  triangle.normal += spin;
+  // Compute cursor angle from triangle normal
+  const dx = mouseX - x;
+  const dy = mouseY - y;
+  const dAngle = Math.atan2(dy, dx);
+  const pointerAlignmentAngle = Math.abs(
+    Math.atan2(Math.sin(normal - dAngle), Math.cos(normal - dAngle))
+  );
+  // Compute distance falloff factor
+  const distance = Math.sqrt(dx ** 2 + dy ** 2);
+  const luminocity = canvas.width * 0.75;
+  const minDistanceFactor = 0.2;
+  let distanceFactor = (luminocity - distance) / luminocity;
+  distanceFactor =
+    distanceFactor < minDistanceFactor ? minDistanceFactor : distanceFactor;
+  // Compute reflectance strength
+  const reflectanceStrength =
+    ((Math.PI - pointerAlignmentAngle) / Math.PI) * distanceFactor;
+  // Triangle path shape setup
+  ctx.beginPath();
+  ctx.lineTo(x + size * Math.sin(normal), y + size * Math.cos(normal));
+  ctx.lineTo(
+    x + size * Math.sin(normal + TAU * 0.333),
+    y + size * Math.cos(normal + TAU * 0.333)
+  );
+  ctx.lineTo(
+    x + size * Math.sin(normal + TAU * 0.666),
+    y + size * Math.cos(normal + TAU * 0.666)
+  );
+  ctx.closePath();
+  // Fill triangle
+  const hue =
+    (pointerAlignmentAngle / Math.PI) * hueRange + hueShift + tHueShift;
+  const sauration = 90;
+  const lightness = (reflectanceStrength * 0.4 + 0.55) * 100;
+  const alpha = reflectanceStrength;
+  ctx.fillStyle = `hsla(${hue},${sauration}%,${lightness}%,${alpha})`;
+  ctx.fill();
+}
+
+// Pointer animation before interaction
+let pointerLoopRAF;
+function pointerLoop(t) {
+  const radiusRatio = 0.35;
+  const radius = canvas.width * radiusRatio;
+  const rate = 0.001;
+  const x =
+    canvas.width * radiusRatio * Math.sin(t * rate) + canvas.width * 0.5;
+  const y =
+    canvas.height * radiusRatio * Math.cos(t * rate) + canvas.height * 0.5;
+  mouseX = x;
+  mouseY = y;
+  pointerLoopRAF = window.requestAnimationFrame(pointerLoop);
+}
+pointerLoop();
+
+// Interactions and Helper functions
+canvas.addEventListener("touchmove", handleInteraction, false);
+canvas.onmousemove = handleInteraction;
+
 function handleInteraction(e) {
+  // Stop the automated cursor animation
   window.cancelAnimationFrame(pointerLoopRAF);
+  // Update global cursor coordinate variables
   const { x, y } = getMousePos(this, e);
   mouseX = x;
   mouseY = y;
 }
 
-canvas.addEventListener("touchmove", handleInteraction, false);
-canvas.onmousemove = handleInteraction;
+function gridPoints(gridSeparation = 64, type = "square") {
+  // type 'square', 'isometric'
+  const coordinates = [];
 
-/// the main function
+  let dy = gridSeparation;
+  if (type === "isometric") {
+    dy = Math.sin((TAU * 60) / 360) * gridSeparation;
+  }
+
+  for (let i = 0; i - 1 < canvas.height / dy; i++) {
+    for (let j = 0; j - 1 < canvas.width / gridSeparation; j++) {
+      let x = j * gridSeparation;
+      if (type === "isometric") {
+        x = !!(i % 2)
+          ? j * gridSeparation
+          : j * gridSeparation + gridSeparation * 0.5;
+      }
+      let y = i * dy;
+      coordinates.push({ x, y, ix: j, iy: i });
+    }
+  }
+
+  return coordinates;
+}
+
 function getMousePos(canvas, e) {
   let x;
   let y;
@@ -223,4 +192,42 @@ function getMousePos(canvas, e) {
   return { x, y };
 }
 
-draw();
+// Unused but useful and reference
+function drawCoordinatePoints(points) {
+  points.forEach(({ x, y }) => {
+    ctx.beginPath();
+    ctx.arc(x, y, 2, 0, TAU);
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+    ctx.closePath();
+  });
+}
+
+const canvasGlobalCompositeOperationOptions = [
+  "source-over",
+  "source-atop",
+  // "source-in",
+  // "source-out",
+  // "destination-over",
+  // "destination-atop",
+  // "destination-in",
+  // "destination-out",
+  // "lighter",
+  // "copy",
+  "xor",
+  "multiply",
+  "darken",
+  // "lighten",
+  // "screen", // 🤷‍
+  // "color-dodge",
+  // "color-burn", // 🤷‍
+  // "hard-light",
+  // "soft-light", // 🤷‍
+  // "difference", // 🤮
+  // "exclusion", // 🤮
+  // "hue", // 🤷‍
+  // "saturation", // 🤷‍
+  // "color", // 🤷‍
+  // "luminosity", // 🤮
+  // "overlay", // 🤷‍
+];
